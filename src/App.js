5efcd350c31cb769cd684056942303cadfea3b62
karthik-mohan <68692486+karthik-mohan@users.noreply.github.com>
2024-02-19 04:39:59 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import Header from './components/Header';
-import HeroSection from './components/HeroSection';
+//import HeroSection from './components/HeroSection';
 import HowItWorks from './components/HowItWorks';
-import CollectionsShowcase from './components/CollectionsShowcase';
+//import CollectionsShowcase from './components/CollectionsShowcase';
 import TryOnFeatureDetails from './components/TryOnFeatureDetails';
 import ContactUs from './components/ContactUs';
 import UserGallery from './components/UserGallery'; // Assume you've implemented this based on previous instructions
 import Footer from './components/Footer'; // Assume you've implemented this based on previous instructions
+import Testimonials from './components/Testimonials';
+import Features from './components/Features';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="hero-background">
       <Header className="fixed w-full bg-black z-10 ..." />
-      <main className="pt-70 ...">
-        <HeroSection />
-  
+      
+        <HomePage />
+      
     <div className="rest-of-the-page">
+      <Features />
       <HowItWorks />
-      <CollectionsShowcase />
       <TryOnFeatureDetails />
+      <Testimonials />
       <UserGallery />
       <ContactUs />
       <Footer />
     </div>
-    </main>
+    
+  
      </div>
    
   
